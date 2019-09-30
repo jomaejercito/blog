9e12492ddf859c71d76f337fa560a185c97730a0
Joma Ejercito <jomaejercito@gmail.com>
2019-09-30 19:19:26 -0400

    Refactor to solve overfetching and finding unique user Ids.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,13 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
-export const fetchPostsAndUsers = () => async dispatch => {
-  console.log('About to fetch posts!');
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
-  console.log('Fetched posts!')
+  const userIds = _.uniq(_.map(getState().posts, 'userId'))
+  userIds.forEach(id => dispatch(fetchUser(id)))
 };
 
-export const fetchPosts = () =>  async (dispatch) => {
+export const fetchPosts = () =>  async (dispatch,) => {
   const response = await jsonPlaceholder.get('/posts');
 
   dispatch ({ type: 'FETCH_POSTS', payload: response.data });
